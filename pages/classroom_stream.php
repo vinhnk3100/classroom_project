20855cc35b3f99c35a1a71569bb18821f47f3ebd
vinhnk3100 <vinhnk2300@gmail.com>
2020-11-12 13:00:11 +0700

    Update
    comment post

--- pages/classroom_stream.php
+++ pages/classroom_stream.php
@@ -34,6 +34,7 @@ require("Initials.php");
     <title>Classroom</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/classroom-stream.css">
@@ -158,7 +159,7 @@ require("Initials.php");
         <!--=================================================================================================================-->
     </div>
 
-    <!--====== COMMENT ========================================-->
+    <!--====== POST CREATE ========================================-->
     <div class="classuis hide_comment_area">
         <div class="comment-area">
             <div class="nav-link" aria-haspopup="true" aria-expanded="false">
@@ -187,18 +188,70 @@ require("Initials.php");
         </div>
     </div>
 
+
+    <!--====== END POST CREATE ========================================-->
+
+    <!--====== POST ========================================-->
     <div class="classuis">
-        <div class="comment-content">
-            <div class="nav-link" aria-haspopup="true" aria-expanded="false">
+        <div class="post">
+            <!--====== POST CREATOR ========================================-->
+            <div class="nav-link p-l-26" aria-haspopup="true" aria-expanded="false">
                 <?php
                 $initials = new Initials();
                 $generateName = $initials->generate($_SESSION['fullname']);
+                echo "<div class='post_user_name'>Nguyen Van A</div>";
                 echo "<div class='circle circle-avt-comments'><div class='initials'>$generateName</div></div>"
                 ?>
             </div>
 
+            <!--====== POST CONTENTS ========================================-->
+            <div class="post_content">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
+                type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+                essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </div>
+            <hr>
+
+            <!--====== POST COMMENTS ========================================-->
+            <div class="post_expand_comments">Click to see more comments....</div>
+            <div class="post_comments">
+                <div class="nav-link" aria-haspopup="true" aria-expanded="false">
+                    <?php
+                    $initials = new Initials();
+                    $generateName = $initials->generate($_SESSION['fullname']);
+                    echo "<div class='post_user_name'>Nguyen Van A</div>";
+                    echo "<div class='circle circle-avt-comments avt_in_post'><div class='initials name_in_post'>$generateName</div></div>";
+                    echo "<div class='post_comments_users'>Lorem Ipsum is psum ipsum ipsum isimply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</div>";
+                    ?>
+                </div>
+            </div>
+            <!--======END POST COMMENTS ========================================-->
+
+            <hr>
+
+            <!--====== CREATE POST COMMENTS ========================================-->
+
+            <div class="nav-link p-l-26" aria-haspopup="true" aria-expanded="false">
+                <?php
+                $initials = new Initials();
+                $generateName = $initials->generate($_SESSION['fullname']);
+                echo "<div class='circle circle-avt-comments avt_in_post'><div class='initials name_in_post'>$generateName</div></div>";
+                echo "
+                <form action='' method='post' class='form_post_comments'>
+                    <div class='input_comments' contenteditable='true' data-text='Say something here....'></div>
+                    <button class='post_comments_btn' type='submit'><i class='fa fa-paper-plane' aria-hidden=\"true\"></i></button>
+                </form>
+                ";
+                ?>
+            </div>
+
+            <!--====== CREAT POST COMMENTS ========================================-->
         </div>
     </div>
+
+    <!--====== END POST ========================================-->
     <br><br><br>
 
 
